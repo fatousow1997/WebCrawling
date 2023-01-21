f1e8ed8ea49f23da2e71aa7b0cf6b2b59452db91
cheikhtidian <90853437+cheikhtidian@users.noreply.github.com>
2023-01-21 17:30:27 +0000

    add file Maquillage.java and OrdinateurImprimante.java

---
+++ CodeBackend/WebCrawling/src/main/java/ProjetNoSQL/JumiaWebCrawling/Model/Informatique/OrdinateurImprimante.java
@@ -0,0 +1,22 @@
+package ProjetNoSQL.JumiaWebCrawling.Model.Informatique;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "ordinateurimprimante")
+public class OrdinateurImprimante {
+    @Id
+    private  String _id;
+    private  String image;
+    private  String name;
+    private  String new_price;
+    private  String old_price;
+}
